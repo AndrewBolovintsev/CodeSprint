236646edb24bbda01428ee40e6a3562d6a93a86f
Andrei <basoo2512@gmail.com>
2025-01-12 23:46:38 +0300

    create project CodeSprint

---
+++ CodeSprint/CodeSprint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeSprint
+//
+//  Created by Andrei on 12.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodeSprint/CodeSprintTests/CodeSprintTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeSprintTests.swift
+//  CodeSprintTests
+//
+//  Created by Andrei on 12.01.25.
+//
+
+import Testing
+@testable import CodeSprint
+
+struct CodeSprintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
